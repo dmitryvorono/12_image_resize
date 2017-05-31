b687b62e0bfa621c99126ad0899233e6b3ec3dd7
dvoronov <voronov-d88@yandex.ru>
2017-05-31 08:20:47 +0300

    Add parser command-line arguments

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,23 @@
+import argparse
+import sys
 
 
-def resize_image(path_to_original, path_to_result):
+def create_parser():
+    parser = argparse.ArgumentParser(description='This script resize images')
+    parser.add_argument('image', type=str, help='Path to file will resize')
+    parser.add_argument('-o', '--output', type=str, help='Path to output file')
+    parser.add_argument('--width', type=int, help='width result image')
+    parser.add_argument('--height', type=int, help='height result image')
+    parser.add_argument('--scale', type=float, help='scale result image')
+    return parser
+
+
+def resize_image(image, output, width, height, scale):
     pass
-
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    args = parser.parse_args()
+    if(args.width or args.height) and args.scale:
+        sys.exit('--wigth and --height|--scale are mutually exclusive')
+    resize_image(**vars(args))
