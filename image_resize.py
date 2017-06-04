6a9c1a4780305e1903cdb41e7a053f5cc2235033
dvoronov <voronov-d88@yandex.ru>
2017-06-04 20:42:20 +0300

    Big refactoring

--- image_resize.py
+++ image_resize.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+import os
+from PIL import Image
 
 
 def create_parser():
@@ -12,12 +14,53 @@ def create_parser():
     return parser
 
 
-def resize_image(image, output, width, height, scale):
-    pass
+def rescale_image(im, scale):
+    return im.resize((int(size * scale) for size in im.size))
+
+
+def resize_image(im, width, height):
+    proportional = im.size[0]/im.size[1]
+    if width and not height:
+        height = round(width / proportional)
+    elif height and not width:
+        width = round(height * proportional)
+    new_size = (width, height)
+    return im.resize(new_size)
+
+
+def add_suffix_to_filename(filepath, suffix):
+        folder, filename = os.path.split(filepath)
+        name, extension = filename.split('.')
+        return ''.join([folder, name, suffix, '.', extension])
+
+
+def is_equal_proportional(old_image, new_image):
+    old_size = old_image.size
+    new_size = new_image.size
+    if old_size[0] / old_size[1] != new_size[0] / new_size[1]:
+        return False
+    else:
+        return True
+
+
+def main(image, output, width, height, scale):
+    im = Image.open(image)
+    if width or height:
+        new_image = resize_image(im, width, height)
+    elif scale:
+        new_image = rescale_image(im, scale)
+    if not is_equal_proportional(im, new_image):
+        print('Warning: proportinal new image is not equal to the original image')
+    if not output:
+        output = add_suffix_to_filename(image, '__{0}x{1}'.format(*new_image.size))
+    new_image.save(output)
+    return 0
+
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
     if(args.width or args.height) and args.scale:
         sys.exit('--wigth and --height|--scale are mutually exclusive')
-    resize_image(**vars(args))
+    status = main(**vars(args))
+    sys.exit(status)
